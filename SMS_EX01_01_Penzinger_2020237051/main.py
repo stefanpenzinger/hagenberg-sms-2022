7f3e16d39409c6c35d188e9e2ea5cd8101416e6b
stefanpenzinger <stefan.penzinger13@gmail.com>
2022-10-11 18:23:39 +0200

    Added enigma

--- SMS_EX01_01_Penzinger_2020237051/main.py
+++ SMS_EX01_01_Penzinger_2020237051/main.py
@@ -1,6 +1,6 @@
 import ex_1_1
 import ex_1_2
-import ex_1_3
+import bonus
 
 
 if __name__ == '__main__':
@@ -8,12 +8,12 @@ if __name__ == '__main__':
     filename = "Chiffre.txt"
     ex_1_1.read_file(filename)
 
-    print("EX 1.2")
+    print("\nEX 1.2")
     play_fair_matrix = ex_1_2.create_playfair_matrix('PASSWORD')
 
     print(play_fair_matrix)
     print(ex_1_2.encrypt('SECURE MESSAGE', play_fair_matrix))
     print(ex_1_2.decrypt('OBRYDRTDBSWSKD', play_fair_matrix))
 
-    print("EX Bonus")
-    print(ex_1_3.enigma(0, 0, 0, "A:H,D:P,X:Y", 'HELLO WORLD'))
+    print("\nEX Bonus")
+    print(bonus.enigma(0, 0, 0, "A:H,D:P,X:Y", 'HELLO WORLD'))
